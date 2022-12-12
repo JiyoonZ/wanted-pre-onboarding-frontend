aca8e0a70a3a800448b033b72ac9683ba176cd32
jiyoonZ <wldbsl416@gmail.com>
2022-12-12 23:16:16 +0900

    Refactor : 컴포넌트 분리

---
+++ src/components/auth/AuthForm.js
@@ -0,0 +1,92 @@
+import {useState, useEffect} from "react";
+import {useNavigate} from "react-router-dom";
+import {Signin, Signup} from "../../lib/api";
+import {validation, validationSignUp} from "../../utils/validation";
+import {setToken} from "../../utils/authToken";
+import * as C from "./style";
+
+const INIT_VAL = {
+  email: "",
+  password: "",
+  checkPassword: "",
+};
+function AuthForm({existUser}) {
+  const [values, setValues] = useState(INIT_VAL);
+  const navigate = useNavigate();
+  const changeHandler = (evt) => {
+    const {name, value} = evt.target;
+    setValues({...values, [name]: value});
+  };
+  useEffect(() => {
+    setValues(INIT_VAL);
+    console.log("chekc!!", existUser);
+  }, [existUser]);
+
+  const sumbitHandler = async (evt) => {
+    evt.preventDefault();
+    const postVal = {email: values.email, password: values.password};
+    if (existUser) {
+      // 기존회원 로그인
+      const resp = await Signin(postVal);
+      setToken(resp.data.access_token);
+      navigate("/todo");
+      console.log(resp, "로그인");
+    } else {
+      //회원가입일때
+      const resp = await Signup(postVal);
+      console.log(resp);
+      // 토큰 담아서 로컬스토리지에 저장하기
+      setToken(resp.data.access_token);
+      navigate("/todo");
+      window.location.href = "/";
+    }
+  };
+  const disabledHandler = () => {
+    if (validation(values)) {
+      return existUser ? false : validationSignUp(values) ? false : true;
+    }
+    return true;
+  };
+  return (
+    <>
+      <form onSubmit={sumbitHandler}>
+        <C.InputGroup>
+          <C.InputTitle>아이디</C.InputTitle>
+          <C.UserInput
+            placeholder="아이디를 입력해주세요 (@포함)"
+            name="email"
+            onChange={changeHandler}
+            required
+          />
+        </C.InputGroup>
+        <C.InputGroup>
+          <C.InputTitle>비밀번호</C.InputTitle>
+          <C.UserInput
+            placeholder="비밀번호를 입력해주세요 (8자 이상입니다)"
+            name="password"
+            onChange={changeHandler}
+            required
+          />
+        </C.InputGroup>
+        {!existUser && (
+          <C.InputGroup>
+            <C.InputTitle>
+              비밀번호
+              <br />
+              확인
+            </C.InputTitle>
+            <C.UserInput
+              placeholder="비밀번호를 다시 입력해주세요"
+              name="checkPassword"
+              onChange={changeHandler}
+              required
+            />
+          </C.InputGroup>
+        )}
+        <C.Button disabled={disabledHandler()}>로그인</C.Button>
+      </form>
+    </>
+  );
+}
+
+export default AuthForm;
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,96 +1,18 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import * as C from "../components/auth/style";
-import {validation, validationSignUp} from "../utils/validation";
 import Container from "../styles/Container";
-import {Signin, Signup} from "../lib/api";
-import {setToken} from "../utils/authToken";
-import {useNavigate} from "react-router-dom";
+import AuthForm from "../components/auth/AuthForm";
 
-const INIT_VAL = {
-  email: "",
-  password: "",
-  checkPassword: "",
-};
 function Auth() {
   const [existUser, setExistUser] = useState(true);
-  const [values, setValues] = useState(INIT_VAL);
-  const navigate = useNavigate();
   const isUser = () => {
     setExistUser((prev) => !prev);
-  };
-  useEffect(() => {
-    setValues(INIT_VAL);
-    console.log("chekc!!");
-  }, [existUser]);
-
-  const changeHandler = (evt) => {
-    const {name, value} = evt.target;
-    setValues({...values, [name]: value});
-  };
-  const sumbitHandler = async (evt) => {
-    evt.preventDefault();
-    const postVal = {email: values.email, password: values.password};
-    if (existUser) {
-      // 기존회원 로그인
-      const resp = await Signin(postVal);
-      navigate("/todo");
-      console.log(resp, "로그인");
-    } else {
-      //회원가입일때
-      const resp = await Signup(postVal);
-      console.log(resp);
-      // 토큰 담아서 로컬스토리지에 저장하기
-      setToken(resp.data.access_token);
-      navigate("/todo");
-      window.location.href = "/";
-    }
-  };
-  const disabledHandler = () => {
-    if (validation(values)) {
-      return existUser ? false : validationSignUp(values) ? false : true;
-    }
-    return true;
   };
 
   return (
     <Container>
       <C.Title>{existUser ? "로그인" : "회원가입"}</C.Title>
-      <form onSubmit={sumbitHandler}>
-        <C.InputGroup>
-          <C.InputTitle>아이디</C.InputTitle>
-          <C.UserInput
-            placeholder="아이디를 입력해주세요 (@포함)"
-            name="email"
-            onChange={changeHandler}
-            required
-          />
-        </C.InputGroup>
-        <C.InputGroup>
-          <C.InputTitle>비밀번호</C.InputTitle>
-          <C.UserInput
-            placeholder="비밀번호를 입력해주세요 (8자 이상입니다)"
-            name="password"
-            onChange={changeHandler}
-            required
-          />
-        </C.InputGroup>
-        {!existUser && (
-          <C.InputGroup>
-            <C.InputTitle>
-              비밀번호
-              <br />
-              확인
-            </C.InputTitle>
-            <C.UserInput
-              placeholder="비밀번호를 다시 입력해주세요"
-              name="checkPassword"
-              onChange={changeHandler}
-              required
-            />
-          </C.InputGroup>
-        )}
-        <C.Button disabled={disabledHandler()}>로그인</C.Button>
-      </form>
+      <AuthForm existUser={existUser} />
       {existUser ? (
         <C.AuthFooter>
           <span>회원이 아니신가요? </span>
