48444f2e8039e029cc790cf214a8de2fc4f755a1
jiyoonZ <wldbsl416@gmail.com>
2022-12-13 20:59:24 +0900

    Feat : 초기화면

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,14 +1,14 @@
 import Container from "../styles/Container";
 import styled from "styled-components";
 import {useEffect, useRef, useState} from "react";
-import {getTodos, postTodo} from "../lib/api";
+import {getTodos, postTodo} from "../api/api";
 import TodoItem from "../components/todo/TodoItem";
 import TodoForm from "../components/todo/TodoForm";
 import * as C from "../components/auth/style";
 import {removeToken} from "../utils/authToken";
 import {useNavigate} from "react-router-dom";
 function Todo() {
-  const [todos, setTodos] = useState();
+  const [todos, setTodos] = useState([]);
   const navigate = useNavigate();
   useEffect(() => {
     refresh();
@@ -27,10 +27,13 @@ function Todo() {
       <Title>Todo List</Title>
       <TodoForm refresh={refresh} />
       <TodoList>
-        {todos &&
+        {todos.length ? (
           todos.map((todo) => (
             <TodoItem key={todo.id} todo={todo} refresh={refresh} />
-          ))}
+          ))
+        ) : (
+          <EmptyBox>할일을 추가해주세요!</EmptyBox>
+        )}
       </TodoList>
     </Container>
   );
@@ -51,5 +54,10 @@ const Logout = styled.p`
   color: ${(props) => props.theme.accentColor};
   text-decoration: underline;
   cursor: pointer;
+`;
+const EmptyBox = styled.p`
+  color: rgba(0, 0, 0, 0.5);
+  text-align: center;
+  margin-top: 40px;
 `;
 export default Todo;
